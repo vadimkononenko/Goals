bd18f1d23a386661a9dcb4aec6754a5558e84452
vadimkononenko <vadiskon@gmail.com>
2023-03-05 20:14:12 +0200

    Initial Commit

---
+++ Goals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goals
+//
+//  Created by Vadim Kononenko on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
